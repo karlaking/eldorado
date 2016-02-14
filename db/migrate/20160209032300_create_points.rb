b6b9c617d5137a756d2e753909a195399c0e4d49
Karla <karlaking2@gmail.com>
2016-02-14 10:23:05 -0800

    Add descriptions to ground truth points

--- db/migrate/20160209032300_create_points.rb
+++ db/migrate/20160209032300_create_points.rb
@@ -2,7 +2,7 @@ class CreatePoints < ActiveRecord::Migration
   def change
     create_table :points do |t|
       t.integer :point_num
-      t.integer :elevation
+      t.string :elevation
       t.string :datetime
       t.text :description
       t.text :img_url
