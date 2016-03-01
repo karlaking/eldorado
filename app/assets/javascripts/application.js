c526dd24c5bfef900d816da06f8580e5da723dca
karlaking <karlaking2@gmail.com>
2016-02-29 18:01:06 -0800

    Update routes to include portfolio page and about me holder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGFraW5nMiIsImEiOiJjaWV1d29pZ2QwczhwczltMm1lbWMxZnJmIn0._CLLgaefzXvhtCeSs61tzQ';
+  L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGFraW5nMiIsImEiOiJjaWt0M2hobmgwMDI0dXltM3d3dGprcDU5In0.N37dBdhmjA55gcjcwk1WdQ';
   var map = L.mapbox.map('map', 'mapbox.satellite')
       .setView([38.805, -120.333], 10);
     gon.points.forEach ( function (point) {
@@ -9,7 +9,7 @@ $(document).ready(function(){
           'marker-color': '#cc33ff',
           'marker-symbol': 'marker-stroked'
         }),
-      }).bindPopup('<p>'+ '<span style="font-size: 2em;">' + point.point_num + '</span>'+ '</p>' + '<p>' +'<span style="font-size: .8em;">' + "Elevation:" + point.elevation + '</p>' + '</span>'+'<img src='+point.img_url+'/>' + '<p>' + "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." + '</p>',
+      }).bindPopup('<p>'+ '<span style="font-size: 2em;">' + point.point_num + '</span>'+ '</p>' + '<p>' +'<span style="font-size: .8em;">' + "Elevation: " + point.elevation + '</p>' + '</span>'+'<img src='+point.img_url+'/>' + '<p>' + point.description + '</p>',
       {autoPanPadding: new L.Point(0, 50)}).addTo(map);
     });
   //  })
