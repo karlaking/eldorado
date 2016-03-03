f7ed17e7075bf346c008af2fc4c1507ff8e16604
karlaking <karlaking2@gmail.com>
2016-03-03 09:49:59 -0800

    Update file structure, refactor js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,16 @@
-$(document).ready(function(){
-  L.mapbox.accessToken = 'pk.eyJ1Ijoia2FybGFraW5nMiIsImEiOiJjaWt0M2hobmgwMDI0dXltM3d3dGprcDU5In0.N37dBdhmjA55gcjcwk1WdQ';
-  var map = L.mapbox.map('map', 'mapbox.satellite')
-      .setView([38.805, -120.333], 10);
-    gon.points.forEach ( function (point) {
-      L.marker([point.latitude, point.longitude], {
-        icon: L.mapbox.marker.icon({
-          'marker-size': 'large',
-          'marker-color': '#cc33ff',
-          'marker-symbol': 'marker-stroked'
-        }),
-      }).bindPopup('<p>'+ '<span style="font-size: 2em;">' + point.point_num + '</span>'+ '</p>' + '<p>' +'<span style="font-size: .8em;">' + "Elevation: " + point.elevation + '</p>' + '</span>'+'<img src='+point.img_url+'/>' + '<p>' + point.description + '</p>',
-      {autoPanPadding: new L.Point(0, 50)}).addTo(map);
-    });
-  //  })
-});
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
 
