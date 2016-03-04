f61be70d34582316fa3b3caf9954abee941a7966
karlaking <karlaking2@gmail.com>
2016-03-03 22:33:30 -0800

    Update image composite for better visualization

--- app/assets/javascripts/eldorado.js
+++ app/assets/javascripts/eldorado.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       L.marker([point.latitude, point.longitude], {
         icon: L.mapbox.marker.icon({
           'marker-size': 'large',
-          'marker-color': '#cc33ff',
+          'marker-color': '#e50000',
           'marker-symbol': 'marker-stroked'
         }),
       }).bindPopup('<p>'+ '<span style="font-size: 2em;">' + point.point_num + '</span>'+ '</p>' + '<p>' +'<span style="font-size: .8em;">' + "Elevation: " + point.elevation + '</p>' + '</span>'+'<img src='+point.img_url+'/>' + '<p>' + point.description + '</p>',
@@ -15,40 +15,14 @@ $(document).ready(function(){
     });
   //  })
     var layers = {
-      Landsat2014: L.mapbox.tileLayer('karlaking2.6d37t8n9', {format: 'png128'}),
-      Landsat2015: L.mapbox.tileLayer('karlaking2.9i6e1tpk', {format: 'png128'}),
+      Landsat2014: L.mapbox.tileLayer('karlaking2.9fwnbjys', {format: 'png128'}),
+      Landsat2015: L.mapbox.tileLayer('karlaking2.du1h8s5z', {format: 'png128'})
 
   };
 
-  layers.Landsat2014.addTo(map);
+  layers.Landsat2015.addTo(map);
   L.control.layers(layers).addTo(map);
 
 });
-
-var myLayer = gon.points
-myLayer.setGeoJSON(geoJson);
-
-// Listen for individual marker clicks.
-myLayer.on('click',function(e) {
-    // Force the popup closed.
-    e.layer.closePopup();
-
-    var feature = e.layer.feature;
-    var content = '<div><strong>' + feature.properties.title + '</strong>' +
-                  '<p>' + feature.properties.description + '</p></div>';
-
-    info.innerHTML = content;
-});
-
-// Clear the tooltip when map is clicked.
-map.on('move', empty);
-
-// Trigger empty contents when the script
-// has loaded on the page.
-empty();
-
-function empty() {
-  info.innerHTML = '<div><strong>Click a marker</strong></div>';
-  }
 
 
